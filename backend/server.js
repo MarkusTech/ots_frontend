62a9b1075436078c80d7d7a28da72f8c1700aa7a
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-16 09:24:27 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import otsRoutes from "./routes/otsRoutes.js";
 import draftNumberRoutes from "./routes/draftNumberRoutes.js";
+import entryNumberRoutes from "./routes/entryNumberRoutes.js";
 
 // dotenv config
 dotenv.config();
@@ -37,6 +38,7 @@ app.get("/", (req, res) => {
 app.use("/api/v1", productRoutes);
 app.use("/api/v1", otsRoutes);
 app.use("/api/v1", draftNumberRoutes);
+app.use("/api/v1,", entryNumberRoutes);
 
 // event listener
 app.listen(port, () => {
