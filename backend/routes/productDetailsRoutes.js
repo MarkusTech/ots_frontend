e0082402c128a865640118d3be81967cf4e63c69
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-16 09:42:43 +0800

    Update productDetailsRoutes.js

--- backend/routes/productDetailsRoutes.js
+++ backend/routes/productDetailsRoutes.js
@@ -1,5 +1,11 @@
 import express from "express";
-
+import {
+  productDetailsSave,
+  getProductDetails,
+} from "../controllers/productDetailsCtrl.js";
 const router = express.Router();
+
+router.post("/product-detail", productDetailsSave);
+router.get("/product-detail", getProductDetails);
 
 export default router;
