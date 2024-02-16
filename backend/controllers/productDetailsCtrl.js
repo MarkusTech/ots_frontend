d22ee51bc8c3866de5cec884efa8f863be46fc7b
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-16 09:41:09 +0800

    backend

---
+++ backend/controllers/productDetailsCtrl.js
@@ -0,0 +1,9 @@
+// import detailModel from "../models/detailModel.js";
+
+const productDetailsSave = async (req, res) => {
+  req.send("Save Product Details Successfully");
+};
+
+const getProductDetails = async (req, res) => {
+  res.send("Get Product Details in an Array");
+};
