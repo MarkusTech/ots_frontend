5e9ed97da6677e1715634a73dc5b6fe027ac2d51
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-16 13:26:21 +0800

    Update SalesOrder.tsx

--- src/app/Transaction/SalesOrder/SalesOrder.tsx
+++ src/app/Transaction/SalesOrder/SalesOrder.tsx
@@ -302,39 +302,86 @@ export default function SalesOrder() {
   ]);
 
   // Handle Draft Submit && Handle Payment Validation
-  const handleSubmit = () => {
-    const validateTable = [...tableData];
+  // const handleSubmit = () => {
+  //   const validateTable = [...tableData];
 
-    if (formData.CustomerCode == "") {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Need to Select Customer First!",
-      });
-    } else if (validateTable[0]["itemCode"] == "") {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Need to Select Atleast 1 Product!",
-      });
-    } else if (
-      isPaymentCash == "N" &&
-      isPaymentCreditCard == "N" &&
-      isPaymentDebitCard == "N" &&
-      isPaymentODC == "N" &&
-      isPaymentPDC == "N" &&
-      isPaymentPO == "N" &&
-      isPaymentOnlineTransfer == "N" &&
-      setOnAccount == "N" &&
-      isPaymentCOD == "N"
-    ) {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Need to Select Payment Method!",
-      });
+  //   if (formData.CustomerCode == "") {
+  //     Swal.fire({
+  //       icon: "error",
+  //       title: "Oops...",
+  //       text: "Need to Select Customer First!",
+  //     });
+  //   } else if (validateTable[0]["itemCode"] == "") {
+  //     Swal.fire({
+  //       icon: "error",
+  //       title: "Oops...",
+  //       text: "Need to Select Atleast 1 Product!",
+  //     });
+  //   } else if (
+  //     isPaymentCash == "N" &&
+  //     isPaymentCreditCard == "N" &&
+  //     isPaymentDebitCard == "N" &&
+  //     isPaymentODC == "N" &&
+  //     isPaymentPDC == "N" &&
+  //     isPaymentPO == "N" &&
+  //     isPaymentOnlineTransfer == "N" &&
+  //     setOnAccount == "N" &&
+  //     isPaymentCOD == "N"
+  //   ) {
+  //     Swal.fire({
+  //       icon: "error",
+  //       title: "Oops...",
+  //       text: "Need to Select Payment Method!",
+  //     });
+  //   } else {
+  //     showAlert();
+  //   }
+  // };
+
+  const handleSubmit = () => {
+    const finalTotalListArr = [...finalTotalList];
+    const arrList = finalTotalListArr[0];
+
+    const allItemsArr = [...tableData];
+    const allItemsArrLen = allItemsArr.length;
+
+    let countAllreleasing = 0;
+
+    console.log("mode of rel", finalTotalList);
+
+    // mode of realeasing
+    for (let i = 0; i < allItemsArrLen; i++) {
+      console.log(allItemsArr[i]["modeOfReleasing"]);
+      if (allItemsArr[i]["modeOfReleasing"] == "") {
+      } else {
+        countAllreleasing++;
+      }
+    }
+
+    if (countAllreleasing == allItemsArrLen) {
+      let countStatusInventory = 0;
+
+      for (let ii = 0; ii < allItemsArrLen; ii++) {
+        if (allItemsArr[ii]["inventoryStatus"] == "Out of Stocks") {
+          countStatusInventory++;
+        }
+      }
+
+      if (countStatusInventory <= 0) {
+        console.log("rmrmrmrmr");
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Please make sure all products are available",
+        });
+      }
     } else {
-      showAlert();
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please make sure all products have mode of releasing",
+      });
     }
   };
 
