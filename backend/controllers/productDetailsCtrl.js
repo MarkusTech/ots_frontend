1fc285b901fbbe2566a6782fd71b062c07b361a4
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-17 10:02:32 +0800

    Update productDetailsCtrl.js

--- backend/controllers/productDetailsCtrl.js
+++ backend/controllers/productDetailsCtrl.js
@@ -1,11 +1,45 @@
 import detailModel from "../models/detailModel.js";
 
 const productDetailsSave = async (req, res) => {
+  const {
+    LineID,
+    EntryNum,
+    ItemCode,
+    ItemName,
+    Quantity,
+    Uom,
+    UoMConv,
+    Whse,
+    InvStat,
+    SellPriceBeDisc,
+    DiscRate,
+    SellPriceAftDisc,
+    LowerBound,
+    TaxCode,
+    TaxCodePerc,
+    TaxAmt,
+    BelPriceDisc,
+    Cost,
+    BelCost,
+    ModeReleasing,
+    SCPWDdisc,
+    GrossTotal,
+  } = req.body;
   req.send("Save Product Details Successfully");
 };
 
 const getProductDetails = async (req, res) => {
-  res.send("Get Product Details in an Array");
+  try {
+    const data = await detailModel.find();
+    res.status(200).json({
+      success: true,
+      message: "Product Detail Fetched",
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error!" });
+  }
 };
 
 export { productDetailsSave, getProductDetails };
