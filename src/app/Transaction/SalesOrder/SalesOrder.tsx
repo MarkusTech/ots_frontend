f4164a41316a644310b32d1f97ed69dc2af4d420
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-21 14:25:58 +0800

    Update SalesOrder.tsx

--- src/app/Transaction/SalesOrder/SalesOrder.tsx
+++ src/app/Transaction/SalesOrder/SalesOrder.tsx
@@ -317,11 +317,6 @@ export default function SalesOrder() {
     axios.post(apiUrl);
   };
 
-  // const entryNumberPOSTAPI = () => {
-  //   const apiUrl = "http://172.16.10.169:5000/api/v1/entrynum";
-  //   axios.post(apiUrl);
-  // };
-
   // -------------------------------------- End of <WMR CODE> header insertion --------------------------------------
 
   // ------------------------------------------ Product Details insertion -------------------------------------------
