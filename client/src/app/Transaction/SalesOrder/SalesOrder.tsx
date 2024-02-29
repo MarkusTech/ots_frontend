fdda2703068fdbf62110aee95e78ce4024c3b363
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-02-29 15:20:12 +0800

    Update SalesOrder.tsx

--- client/src/app/Transaction/SalesOrder/SalesOrder.tsx
+++ client/src/app/Transaction/SalesOrder/SalesOrder.tsx
@@ -3473,7 +3473,6 @@ export default function SalesOrder() {
             <button
               className="p-2 mt-2 mb-1 mr-2 text-[12px] bg-[#F4D674] hover:bg-yellow-500 focus:outline-none focus:shadow-outline-yellow active:bg-yellow-600 rounded w-24"
               // onClick={wmrCustomer}
-              onClick={onAddSaveDraft}
             >
               Commit
             </button>
