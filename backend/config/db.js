1093fb89e90e84bd6085cdc17dc01dcaa151e7a8
MarkusTech <wm.recopelacion@gmail.com>
2024-03-01 13:49:56 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,3 @@
-// import mongoose from "mongoose";
-// import colors from "colors";
-
-// const connectDB = async () => {
-//   try {
-//     const connect = await mongoose.connect(process.env.MONGODB);
-//     console.log(
-//       `Database connected on ${connect.connection.host} ${connect.connection.name}`
-//         .bgGreen
-//     );
-//   } catch (err) {
-//     console.log(err);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
 import sql from "mssql";
 import dotenv from "dotenv";
 dotenv.config();
