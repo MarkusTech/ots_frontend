9ba7960c9f3d10f7c46a39286aa76a6d9d543add
MarkusTech <wm.recopelacion@gmail.com>
2024-03-01 14:21:18 +0800

    backend for getting draft data via draftNumber

--- backend/controllers/getSingleDraftData.js
+++ backend/controllers/getSingleDraftData.js
@@ -1,7 +1,13 @@
 import sqlConn from "../config/db.js";
 
 const getSingleDataFromDraft = async (req, res) => {
-  res.send("Hello World!");
+  try {
+    const data = await sqlConn.query("Select * From SO_Header");
+    res.status(200).json(data.recordset);
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
 };
 
 export { getSingleDataFromDraft };
