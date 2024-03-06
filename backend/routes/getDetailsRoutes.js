cd56fc3a49e74004fbc0df124e60001a55e5df36
MarkusTech <wm.recopelacion@gmail.com>
2024-03-06 10:50:29 +0800

    Update getDetailsRoutes.js

--- backend/routes/getDetailsRoutes.js
+++ backend/routes/getDetailsRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import {
   getDetails,
-  getSingleDetails,
+  getSelectedDetails,
 } from "../controllers/getDetailsController.js";
 const router = express.Router();
 
 router.get("/get-details", getDetails);
-router.get("/get-detail/:DraftNum", getSingleDetails);
+router.get("/get-detail/:DraftNum", getSelectedDetails);
 
 export default router;
