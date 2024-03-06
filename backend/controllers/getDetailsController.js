9eaa946dd04cd409ca29f1f4172759cf0a45034c
MarkusTech <wm.recopelacion@gmail.com>
2024-03-06 10:35:01 +0800

    Update getDetailsController.js

--- backend/controllers/getDetailsController.js
+++ backend/controllers/getDetailsController.js
@@ -17,9 +17,20 @@ const getDetails = async (req, res) => {
 const getSingleDetails = async (req, res) => {
   try {
     const { DraftNum } = req.params;
+    const result = await sqlConn.query(
+      `Select * from SO_Details where DraftNum = ${DraftNum}`
+    );
+
+    if (result.recordset.length > 0) {
+      // If a matching record is found, send it in the response
+      res.json(result.recordset[0]);
+    } else {
+      // If no matching record is found, send an appropriate response
+      res.status(404).json({ message: "Record not found" });
+    }
   } catch (error) {
-    console.log(error);
-    throw new Error(error);
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
