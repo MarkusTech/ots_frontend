1e16d9732c968991d682d13dc7608dc2c0191799
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-03-09 13:21:59 +0800

    update SalesOrder

--- client/src/app/Transaction/SalesOrder/SalesOrder.tsx
+++ client/src/app/Transaction/SalesOrder/SalesOrder.tsx
@@ -1152,7 +1152,7 @@ export default function SalesOrder() {
         cusLicTradNum: getDraft.data.TIN,
       },
     ]);
-    setWalkingCustomer(getDraft.data.WalkInName); // walkin
+    setWalkingCustomer(getDraft.data.WalkInName); // walki
     setCustomerReference(getDraft.data.Reference); // Customer Reference
     setScOrPwdField(getDraft.data.SCPWDIdNo); // SC/PWD ID
     setDraftNumber(getDraft.data.DraftNum); // Draft Number
