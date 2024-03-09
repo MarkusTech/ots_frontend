ff54c18a3b64bd73057d1addc7583df640c29246
MarkusTech <wm.recopelacion@gmail.com>
2024-03-09 13:29:51 +0800

    Update SalesOrder.tsx

--- client/src/app/Transaction/SalesOrder/SalesOrder.tsx
+++ client/src/app/Transaction/SalesOrder/SalesOrder.tsx
@@ -1152,7 +1152,7 @@ export default function SalesOrder() {
         cusLicTradNum: getDraft.data.TIN,
       },
     ]);
-    setWalkingCustomer(getDraft.data.WalkInName); // walkin custome
+    setWalkingCustomer(getDraft.data.WalkInName); // walkin customer
     setCustomerReference(getDraft.data.Reference); // Customer Reference
     setScOrPwdField(getDraft.data.SCPWDIdNo); // SC/PWD ID
     setDraftNumber(getDraft.data.DraftNum); // Draft Number
