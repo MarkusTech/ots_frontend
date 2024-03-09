68321efcedfc37a6906b623f51ca609a202531b3
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-03-09 13:27:24 +0800

    update SalesOrder

--- client/src/app/Transaction/SalesOrder/SalesOrder.tsx
+++ client/src/app/Transaction/SalesOrder/SalesOrder.tsx
@@ -1155,7 +1155,7 @@ export default function SalesOrder() {
     setWalkingCustomer(getDraft.data.WalkInName);
     setCustomerReference(getDraft.data.Reference);
     setScOrPwdField(getDraft.data.SCPWDIdNo);
-    setDraftNumber(getDraft.data.DraftNum); //
+    setDraftNumber(getDraft.data.DraftNum);
 
     // Payment Method
     setIsPaymentCash(getDraft.data.Cash);
