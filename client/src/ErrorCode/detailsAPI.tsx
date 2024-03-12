d56082dcd59ae55032f6a02131526831794a3a3a
MarkusTech <wm.recopelacion@gmail.com>
2024-03-12 15:33:26 +0800

    sad

--- client/src/ErrorCode/detailsAPI.tsx
+++ client/src/ErrorCode/detailsAPI.tsx
@@ -3860,3 +3860,43 @@ export default function SalesOrder() {
     </>
   );
 }
+
+{
+  showSalesOrder && (
+    <Draggable handle=".header">
+      <div
+        className="container bg-white"
+        style={{
+          border: "1px solid #ccc",
+          position: "absolute",
+          zIndex: 2,
+          top: "5%",
+          left: "15%",
+          transform: "translate(-50%, -50%)",
+          borderBottom: "solid 2px #F0AB00",
+        }}
+      >
+        <div
+          className="header grid grid-cols-2 p-2 text-left windowheader"
+          style={{
+            cursor: "move",
+            borderBottom: "solid 2px #F0AB00",
+          }}
+        >
+          <div className="">Sales Order Header</div>
+          <div className="text-right">
+            <span
+              className="text-md text-red-600 cursor-pointer"
+              onClick={() => toggleWindow("salesorder")}
+            >
+              ❌
+            </span>
+          </div>
+        </div>
+        <div className="content">
+          <SalesOrder /> {/* Adjust this line accordingly */}
+        </div>
+      </div>
+    </Draggable>
+  );
+}
