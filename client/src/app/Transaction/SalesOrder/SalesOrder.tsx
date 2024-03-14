49d3388a6c1bddac0ded8a87bfb2397b7023a841
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-03-14 09:36:33 +0800

    Update SalesOrder.tsx

--- client/src/app/Transaction/SalesOrder/SalesOrder.tsx
+++ client/src/app/Transaction/SalesOrder/SalesOrder.tsx
@@ -1574,7 +1574,7 @@ export default function SalesOrder() {
 
   let localCurrency = new Intl.NumberFormat("en-PH", {
     style: "currency",
-    currency: "PHP", // Philippines currency code fo
+    currency: "PHP", // Philippines currency code f
   }); // Philippines currency code for Philippine Peso
 
   const handleSearchItem = (event: any) => {
