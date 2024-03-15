261cb4927b6534f517a3bf7d04dd7f475a30eba5
MarkusTech <wm.recopelacion@gmail.com>
2024-03-15 09:48:59 +0800

    Update SalesOrder.tsx

--- client/src/app/Transaction/SalesOrder/SalesOrder.tsx
+++ client/src/app/Transaction/SalesOrder/SalesOrder.tsx
@@ -3680,12 +3680,7 @@ export default function SalesOrder() {
 
             <button
               className="p-2 mt-2 mb-1 mr-2 text-[12px] bg-[#F4D674] hover:bg-yellow-500 focus:outline-none focus:shadow-outline-yellow active:bg-yellow-600 rounded w-24"
-              // onClick={deleteDetailsThenSave}
               onClick={getDetailsAPI}
-              // onClick={() => {
-              //   window.close(); // Close the previous window
-              //   toggleWindow("salesorder"); // Toggle the Sales Order window
-              // }}
             >
               Print
             </button>
