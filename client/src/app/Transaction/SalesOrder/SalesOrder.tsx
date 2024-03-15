e4419fa8eced9655a90db6e8e6cda730840efc78
MarkusTech <wm.recopelacion@gmail.com>
2024-03-15 11:45:44 +0800

    Update SalesOrder.tsx

--- client/src/app/Transaction/SalesOrder/SalesOrder.tsx
+++ client/src/app/Transaction/SalesOrder/SalesOrder.tsx
@@ -1793,16 +1793,6 @@ export default function SalesOrder() {
                 item.sellingPriceBeforeDiscount) *
               100;
 
-            console.log(
-              i,
-              disRateFor,
-              item2.creditcard,
-              item2.debit,
-              item2.pdc,
-              item2.po,
-              "- Done CC"
-            );
-
             const newupdatedTableData = [...tableData];
             const itemnew = newupdatedTableData[i];
 
