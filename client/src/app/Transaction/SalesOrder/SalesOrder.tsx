5426c061d738df172888915d1d6595e6bc74bd95
MarkusTech <wm.recopelacion@gmail.com>
2024-03-16 15:50:46 +0800

    Update SalesOrder.tsx

--- client/src/app/Transaction/SalesOrder/SalesOrder.tsx
+++ client/src/app/Transaction/SalesOrder/SalesOrder.tsx
@@ -3662,7 +3662,7 @@ export default function SalesOrder() {
 
             <button
               className="p-2 mt-2 mb-1 mr-2 text-[12px] bg-[#F4D674] hover:bg-yellow-500 focus:outline-none focus:shadow-outline-yellow active:bg-yellow-600 rounded w-24"
-              onClick={getDetailsAPI}
+              // onClick={}
             >
               Print
             </button>
