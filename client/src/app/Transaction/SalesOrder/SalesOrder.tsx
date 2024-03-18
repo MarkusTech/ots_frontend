3a554a93e2eb3ccc60a6f0f088a5cab19e38c34f
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-03-18 13:14:54 +0800

    update SalesOrder.tsx

--- client/src/app/Transaction/SalesOrder/SalesOrder.tsx
+++ client/src/app/Transaction/SalesOrder/SalesOrder.tsx
@@ -998,7 +998,7 @@ export default function SalesOrder() {
     setSearchTerm("");
 
     // for open and close draggable and for the button save and update
-    // for open and close draggable and for the b
+    // for open and close draggable and for the 
     setShowSearchHeader(!showSearchHeader);
     setIsSaved(true);
   };
