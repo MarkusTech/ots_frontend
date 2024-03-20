a86822f83973c015f1f9a33da0639c5d15ebe64a
MarkusTech <wm.recopelacion@gmail.com>
2024-03-20 11:49:00 +0800

    Update saveCommitRoutes.js

--- backend/routes/saveCommitRoutes.js
+++ backend/routes/saveCommitRoutes.js
@@ -2,4 +2,6 @@ import express from "express";
 import { saveCommit } from "../controllers/saveCommit.js";
 const router = express.Router();
 
+router.post("/save-commit", saveCommit);
+
 export default router;
