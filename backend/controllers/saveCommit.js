5a0677e64892c68e231d9363fc7f7d01da0dc49b
MarkusTech <wm.recopelacion@gmail.com>
2024-03-20 11:53:04 +0800

    Update saveCommit.js

--- backend/controllers/saveCommit.js
+++ backend/controllers/saveCommit.js
@@ -1,7 +1,11 @@
 import sqlConn from "../config/db.js";
 
 const saveCommitHeader = async (req, res) => {
-  res.send("Save Commit");
+  res.send("Save Commit Header");
+};
+
+const saveCommitDetails = async (req, res) => {
+  res.send("Save Commit Details");
 };
 
 export { saveCommitHeader };
