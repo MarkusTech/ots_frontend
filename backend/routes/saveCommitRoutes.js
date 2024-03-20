9ffd967dec2f0ad8c3d57a3d0d30c98b355a4bc8
MarkusTech <wm.recopelacion@gmail.com>
2024-03-20 16:13:03 +0800

    Update saveCommitRoutes.js

--- backend/routes/saveCommitRoutes.js
+++ backend/routes/saveCommitRoutes.js
@@ -3,11 +3,13 @@ import {
   saveCommitHeader,
   saveCommitDetails,
   getAllCommitedHeader,
+  getAllCommitedDetails,
 } from "../controllers/saveCommit.js";
 const router = express.Router();
 
 router.post("/save-commit-header", saveCommitHeader);
 router.post("/save-commit-details", saveCommitDetails);
 router.get("/get-commit-header", getAllCommitedHeader);
+router.get("/get-commit-details", getAllCommitedDetails);
 
 export default router;
