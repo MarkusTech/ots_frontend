add82a335a0b1db510845755a04cc015310ee5c9
MarkusTech <wm.recopelacion@gmail.com>
2024-03-26 13:54:36 +0800

    Update saveFinalCommit.js

--- backend/controllers/saveFinalCommit.js
+++ backend/controllers/saveFinalCommit.js
@@ -5,15 +5,16 @@ const saveCommit = async (req, res) => {
     const docNumberResult =
       await sqlConn.query`SELECT MAX(DocNum) AS maxDocNum FROM SO_Header`;
 
-    const maxDraftNum = parseInt(docNumberResult.recordset[0].maxDocNum);
+    const maxDocNum = parseInt(docNumberResult.recordset[0].maxDocNum);
     const incrementedDraftNum = maxDocNum + 1;
 
-    // const {DocNum, DraftNum} = req.body;
-    // const result = await sqlConn.query`update So_Header set Docnum = ${DocNum} where DraftNum = ${DraftNum}`
+    const { DraftNum } = req.body;
+    const result =
+      await sqlConn.query`update So_Header set DocNum = ${incrementedDraftNum} where DraftNum = ${DraftNum}`;
     res.status(200).json({
       success: true,
       message: "Commited Successfully",
-      incrementedDraftNum,
+      result,
     });
   } catch (error) {
     console.log(error);
