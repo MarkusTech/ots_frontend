0ed091944aa7ef0ff0906ac179509ea8222b489b
MarkusTech <wm.recopelacion@gmail.com>
2024-04-08 10:46:59 +0800

    Update filter.tsx

--- client/src/ErrorCode/filter.tsx
+++ client/src/ErrorCode/filter.tsx
@@ -1,92 +1,87 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-export default function DateFilter() {
-  const [fromDate, setFromDate] = useState("");
-  const [toDate, setToDate] = useState("");
+function YourComponent() {
+  const [tableData, setTableData] = useState([
+    {
+      draftNumber: "",
+      entryNumber: "", // sample
+      itemCode: "",
+      itemName: "",
+      quantity: 0,
+      uom: "",
+      uomConversion: 0,
+      excludeBO: "N",
+      location: "", // <-- This is where we'll store the pick-up location
+      price: 0,
+      inventoryStatus: "",
+      sellingPriceBeforeDiscount: 0,
+      discountRate: 0,
+      sellingPriceAfterDiscount: 0,
+      sellingPriceAfterDiscountTemp: 0,
+      lowerBound: 0,
+      taxCode: "",
+      taxCodePercentage: 0,
+      taxAmount: 0,
+      volDisPrice: 0,
+      belVolDisPrice: 0,
+      cost: 0,
+      belCost: "",
+      modeOfReleasing: "",
+      scPwdDiscount: "N",
+      grossTotal: 0,
+      selected: false,
+      cash: "N",
+      creditcard: "N",
+      debit: "N",
+      pdc: "N",
+      po: "N",
+      datedCheck: "N",
+      onlineTransfer: "N",
+      onAccount: "N",
+      cashOnDel: "N",
+    },
+  ]);
 
-  const handleFilter = () => {
-    // Here you can use the values of fromDate and toDate as needed
-    console.log("From Date:", fromDate);
-    console.log("To Date:", toDate);
-  };
+  // Function to handle change in pick-up location
+  function changeManualPickUpLocation(value) {
+    // Update the pick-up location for the first item in tableData array
+    const updatedTableData = [...tableData];
+    updatedTableData[0].location = value;
+    setTableData(updatedTableData);
+  }
+
+  // Function to save the pick-up location
+  function savePickUpLocation(location) {
+    // Here, you can implement your logic to save the location data in useState
+    // For simplicity, we're directly updating the location field of the first item in tableData
+    const updatedTableData = [...tableData];
+    updatedTableData[0].location = location;
+    setTableData(updatedTableData);
+  }
 
   return (
-    <div className="flex items-center">
-      <label htmlFor="fromDate" className="mr-2">
-        Filter From:
-      </label>
-      <input
-        id="fromDate"
-        type="date"
-        className="w-24 md:w-32 px-2 py-1 border rounded"
-        onChange={(e) => setFromDate(e.target.value)}
-      />
-      <label htmlFor="toDate" className="ml-4 mr-2">
-        To:
-      </label>
-      <input
-        id="toDate"
-        type="date"
-        className="w-24 md:w-32 px-2 py-1 border rounded"
-        onChange={(e) => setToDate(e.target.value)}
-      />
-      <p>:&nbsp;</p>
-      <button
-        className="p-2 mt-2 mb-1 mr-2 text-xs bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:shadow-outline-yellow active:bg-yellow-600 rounded w-24"
-        onClick={handleFilter}
-      >
-        Filter
-      </button>
-    </div>
+    <tbody>
+      <tr>
+        <td>
+          <select
+            name=""
+            className="w-full p-2"
+            onChange={(e) => changeManualPickUpLocation(e.target.value)}
+            id=""
+            value={tableData[0].location} // Ensures the selected value is reflected in the dropdown
+          >
+            <option value="" disabled selected>
+              Please Select
+            </option>
+            <option value="SA">Selling Area</option>
+            <option value="Storage">Storage</option>
+            <option value="Whse">Warehouse</option>{" "}
+            {/* Fixed the typo in "Warehouse" */}
+          </select>
+        </td>
+      </tr>
+    </tbody>
   );
 }
 
-// const PrintReceipt = () => {
-//   const printWindow = window.open("", "_blank");
-
-//   if (printWindow) {
-//     printWindow.document.write(`
-//     <div className="text-center">
-//       <h4>SAFETYBUILD INC</h4>
-//       <h4>DC-SBI GENSAN</h4>
-//       <h3>SALES ORDER STUB </h3>
-//       <h1>${docNumber}</h1>
-//       <h4>${customerPrint}</h4>
-//       <h4>${modeOfPaymentPrint}</h4>
-//       <h4>${modeOfReleasingPrint}</h4>
-//       <h4>GSCNAPGS</h4>
-//       <h4>${selectedSalesCrew}</h4>
-//       <h4>${totalAmoutDueData}</h4>
-//     </div>
-//   `);
-
-//     printWindow.document.close();
-//     printWindow.print();
-//   }
-// };
-
-// const PrintReceipt = () => {
-//   const printWindow = window.open("", "_blank");
-
-//   if (printWindow) {
-//     printWindow.document.write(`
-//           <div style="text-align: center;">
-//               <h4>SAFETYBUILD INC</h4>
-//               <h4>DC-SBI GENSAN</h4>
-//               <h3>SALES ORDER STUB </h3>
-//               <h1>${docNumber}</h1>
-//               <h4>${customerPrint}</h4>
-//               <h4>${modeOfPaymentPrint}</h4>
-//               <h4>${modeOfReleasingPrint}</h4>
-//               <h4>GSCNAPGS</h4>
-//               <h4>${selectedSalesCrew}</h4>
-//               <h4>${totalAmoutDueData}</h4>
-//           </div>
-//       `);
-
-//     printWindow.document.close();
-//     printWindow.print();
-//   }
-// };
-
-// 715
+export default YourComponent;
