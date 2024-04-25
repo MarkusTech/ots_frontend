01321648f21192443a0c9eab07b67d0902e58103
MarkusTech <wm.recopelacion@gmail.com>
2024-04-25 13:43:09 +0800

    Update receipt.js

--- backend/controllers/receipt.js
+++ backend/controllers/receipt.js
@@ -1,1 +1,20 @@
 import sqlConn from "../config/db.js";
+
+const receipt = async (req, res) => {
+  try {
+    const { DraftNumber } = req.params;
+    const result = await sqlConn.query(
+      `SELECT DISTINCT ModeReleasing,dbo.fn_GetSOModeOfRel(DraftNum,ModeReleasing) AS PickUpLocation FROM View_SOReleasing WHERE DraftNUm=${DraftNumber}`
+    );
+    res.status(200).json({
+      success: true,
+      message: "Receipt Record Fetched",
+      result,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export { receipt };
