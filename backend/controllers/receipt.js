403344ab0b04dd0e71a70c218fc5eb09887178ef
MarkusTech <wm.recopelacion@gmail.com>
2024-04-26 15:07:43 +0800

    Update receipt.js

--- backend/controllers/receipt.js
+++ backend/controllers/receipt.js
@@ -6,11 +6,13 @@ const receipt = async (req, res) => {
     const result = await sqlConn.query(
       `SELECT DISTINCT ModeReleasing,dbo.fn_GetSOModeOfRel(DraftNum,ModeReleasing) AS PickUpLocation FROM View_SOReleasing WHERE DraftNUm='${DraftNumber}'`
     );
-    res.status(200).json({
-      success: true,
-      message: "Receipt Record Fetched",
-      result,
-    });
+
+    if (result.recordset.length > 0) {
+      res.json(result.recordset[0]);
+    } else {
+      // If no matching record is found, send an appropriate response
+      res.status(404).json({ message: "Record not found" });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
