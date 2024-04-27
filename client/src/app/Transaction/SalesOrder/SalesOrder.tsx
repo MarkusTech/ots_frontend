621c21b4291687a1a8ef9d4a5ee6762d45c22ff5
Wenn Mark Recopelacion <55816864+MarkusTech@users.noreply.github.com>
2024-04-27 16:24:15 +0800

    update Sales order

--- client/src/app/Transaction/SalesOrder/SalesOrder.tsx
+++ client/src/app/Transaction/SalesOrder/SalesOrder.tsx
@@ -1301,7 +1301,7 @@ export default function SalesOrder() {
           }));
 
           // add the data from selected details
-          // add the
+          // add th
           setTableData([...tableData, ...newData]);
 
           if (newData.length > 0) {
