0535624d434771299c3ec5bf0d24e355b550996c
MarkusTech <wm.recopelacion@gmail.com>
2024-05-09 16:34:15 +0800

    Update page.tsx

--- client/src/app/Setup/Users/page.tsx
+++ client/src/app/Setup/Users/page.tsx
@@ -192,7 +192,7 @@ export default function SignUpPage() {
               left: "20%",
               maxHeight: "700px",
               overflowY: "auto",
-              background: "white", // Set background to white
+              background: "white",
               zIndex: "9999", // Set a high z-index value to bring it to the front. HAHAHA
             }}
           >
