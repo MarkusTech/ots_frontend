5b615459e419ef8a3abb9f83add70bef439c3b17
MarkusTech <wm.recopelacion@gmail.com>
2024-05-13 16:12:41 +0800

    Update db2_saveUsers.js

--- backend/controllers/db2_saveUsers.js
+++ backend/controllers/db2_saveUsers.js
@@ -1,7 +1,42 @@
 import sqlConn2 from "../config/db2.js";
 
 const saveUsers = async (req, res) => {
-  res.send("Save Users");
+  const {
+    EmpName,
+    Position,
+    BranchID,
+    BranchName,
+    WhsCode,
+    PriceListNum,
+    userName,
+    Password,
+  } = req.body;
+
+  // Trim string values if they exceed column sizes
+  const trimmedEmpName = EmpName.substring(0, 50); // Adjust 50 to match column size
+  const trimmedBranchName = BranchName.substring(0, 50); // Adjust 50 to match column size
+  const trimmedUserName = userName.substring(0, 50); // Adjust 50 to match column size
+  const trimmedPassword = Password.substring(0, 50); // Adjust 50 to match column size
+
+  const data = await sqlConn2.query`INSERT INTO [dbo].[User]
+    (EmpName, Position, UserName, Password, BranchID, BranchName, WhsCode, PriceListNum)
+    VALUES
+    (
+      ${trimmedEmpName},
+      ${Position},
+      ${trimmedUserName},
+      ${trimmedPassword},
+      ${BranchID},
+      ${trimmedBranchName},
+      ${WhsCode},
+      ${PriceListNum}
+    )`;
+
+  res.status(200).json({
+    success: true,
+    message: "User Save Successfully",
+    data,
+  });
 };
 
 const getUsers = async (req, res) => {
