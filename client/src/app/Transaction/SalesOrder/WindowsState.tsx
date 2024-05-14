454694ae496738029621d6f83d8ba51c26cfd6f5
MarkusTech <wm.recopelacion@gmail.com>
2024-05-14 14:01:02 +0800

    update

--- client/src/app/Transaction/SalesOrder/WindowsState.tsx
+++ client/src/app/Transaction/SalesOrder/WindowsState.tsx
@@ -5,16 +5,17 @@ import { useState } from "react";
 export const useWindowState = () => {
   const [showSalesOrder, setShowSalesOrder] = useState(false);
   const [showUsers, setShowUsers] = useState(false);
+  const [viewUsers, setViewUsers] = useState(false);
 
   const toggleWindow = (e: any) => {
     if (e === "salesorder") {
       setShowSalesOrder(!showSalesOrder);
     } else if (e === "users") {
       setShowUsers(!showUsers);
-    } else if (e === "settings") {
-      setShowSalesOrder(!showSalesOrder);
+    } else if (e === "view") {
+      setViewUsers(!viewUsers);
     }
   };
 
-  return { showSalesOrder, showUsers, toggleWindow };
+  return { showSalesOrder, showUsers, viewUsers, toggleWindow };
 };
