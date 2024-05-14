1c7d431797ac15543fa9ebe17b8b3be3fc0f4231
MarkusTech <wm.recopelacion@gmail.com>
2024-05-14 16:24:01 +0800

    update frontend

--- client/src/app/Setup/ViewUsers/page.tsx
+++ client/src/app/Setup/ViewUsers/page.tsx
@@ -46,92 +46,88 @@ const ViewPage: React.FC = () => {
 
   return (
     <div className="container mx-auto">
-      <table className="min-w-full divide-y divide-gray-200 shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-        <thead className="bg-gray-50">
-          <tr className="bg-blue-100">
-            <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
-              User ID
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
-              Full Name
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
-              Position
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
-              Branch ID
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
-              Branch Name
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
-              Warehouse Code
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
-              Price List Number
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
-              Username
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
-              Password
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
-              Actions
-            </th>
-          </tr>
-        </thead>
-        <tbody
-          className={
-            users.length > 13
-              ? "bg-white divide-y divide-gray-200 h-96 overflow-y-auto"
-              : "bg-white divide-y divide-gray-200"
-          }
-        >
-          {users.slice(0, 13).map((user: User) => (
-            <tr
-              key={user.UserID}
-              className="hover:bg-blue-100 cursor-pointer transition-colors"
-            >
-              <td className="px-6 py-3 whitespace-nowrap text-sm font-medium text-gray-900">
-                {user.UserID}
-              </td>
-              <td className="px-6 py-3 whitespace-nowrap text-sm text-gray-500">
-                {user.EmpName}
-              </td>
-              <td className="px-6 py-3 whitespace-nowrap text-sm text-gray-500">
-                {user.Position}
-              </td>
-              <td className="px-6 py-3 whitespace-nowrap text-sm text-gray-500">
-                {user.BranchID}
-              </td>
-              <td className="px-6 py-3 whitespace-nowrap text-sm text-gray-500">
-                {user.BranchName}
-              </td>
-              <td className="px-6 py-3 whitespace-nowrap text-sm text-gray-500">
-                {user.WhsCode}
-              </td>
-              <td className="px-6 py-3 whitespace-nowrap text-sm text-gray-500">
-                {user.PriceListNum}
-              </td>
-              <td className="px-6 py-3 whitespace-nowrap text-sm text-gray-500">
-                {user.UserName}
-              </td>
-              <td className="px-6 py-3 whitespace-nowrap text-sm text-gray-500">
-                {user.Password}
-              </td>
-              <td className="px-6 py-3 whitespace-nowrap text-right text-sm font-medium">
-                <button
-                  className="text-indigo-600 hover:text-indigo-900"
-                  onClick={handleShowEdit}
-                >
-                  <EditIcon />
-                </button>
-              </td>
+      <div className="tableHeigthView overflow-y-auto">
+        <table className="min-w-full divide-y divide-gray-200 shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+          <thead className="bg-gray-50">
+            <tr className="bg-blue-100">
+              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
+                User ID
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
+                Full Name
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
+                Position
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
+                Branch ID
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
+                Branch Name
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
+                Warehouse Code
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
+                Price List Number
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
+                Username
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
+                Password
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-700 uppercase">
+                Actions
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {users.map((user: User) => (
+              <tr
+                key={user.UserID}
+                className="hover:bg-blue-100 cursor-pointer transition-colors"
+              >
+                <td className="px-6 py-3 whitespace-nowrap text-sm font-medium text-gray-900">
+                  {user.UserID}
+                </td>
+                <td className="px-6 py-3 whitespace-nowrap text-sm text-gray-500">
+                  {user.EmpName}
+                </td>
+                <td className="px-6 py-3 whitespace-nowrap text-sm text-gray-500">
+                  {user.Position}
+                </td>
+                <td className="px-6 py-3 whitespace-nowrap text-sm text-gray-500">
+                  {user.BranchID}
+                </td>
+                <td className="px-6 py-3 whitespace-nowrap text-sm text-gray-500">
+                  {user.BranchName}
+                </td>
+                <td className="px-6 py-3 whitespace-nowrap text-sm text-gray-500">
+                  {user.WhsCode}
+                </td>
+                <td className="px-6 py-3 whitespace-nowrap text-sm text-gray-500">
+                  {user.PriceListNum}
+                </td>
+                <td className="px-6 py-3 whitespace-nowrap text-sm text-gray-500">
+                  {user.UserName}
+                </td>
+                <td className="px-6 py-3 whitespace-nowrap text-sm text-gray-500">
+                  {user.Password}
+                </td>
+                <td className="px-6 py-3 whitespace-nowrap text-right text-sm font-medium">
+                  <button
+                    className="text-indigo-600 hover:text-indigo-900"
+                    onClick={handleShowEdit}
+                  >
+                    <EditIcon />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       <div className="flex justify-between items-center mb-6 pt-2 px-4">
         <h2 className="text-lg font-semibold text-gray-800"></h2>
         <button
