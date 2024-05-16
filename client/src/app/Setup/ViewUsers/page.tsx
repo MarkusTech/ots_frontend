60feb5773cc8ea9585ec7d947cbc3ac08d6b12c2
MarkusTech <wm.recopelacion@gmail.com>
2024-05-16 15:16:48 +0800

    Update page.tsx

--- client/src/app/Setup/ViewUsers/page.tsx
+++ client/src/app/Setup/ViewUsers/page.tsx
@@ -316,11 +316,247 @@ const ViewPage: React.FC = () => {
             >
               <div>Edit User</div>
               <div className="text-right">
-                <span className="cursor-pointer">❌</span>
+                <span className="cursor-pointer" onClick={handleShowEdit}>
+                  ❌
+                </span>
               </div>
             </div>
             <div className="content">
-              <div className="p-2">asdasd</div>
+              <div className="p-2">
+                <Container component="main" maxWidth="md">
+                  <div>
+                    <br />
+                    <br />
+                    <Typography variant="h4" align="center" gutterBottom>
+                      Edit User
+                    </Typography>
+                  </div>
+                  <form onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          fullWidth
+                          id="userID"
+                          name="userID"
+                          label="UserID"
+                          variant="outlined"
+                          value={formData.userID}
+                          onChange={handleChange}
+                          InputProps={{
+                            readOnly: true,
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={1}>
+                        <Button
+                          // fullWidth
+                          variant="outlined"
+                          onClick={handleButtonClick} // You need to define handleButtonClick function
+                          style={{
+                            height: "100%",
+                            width: "130px",
+                            fontWeight: "bold",
+                          }}
+                        >
+                          Select User
+                        </Button>
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          fullWidth
+                          id="fullName"
+                          name="fullName"
+                          label="Full Name"
+                          variant="outlined"
+                          value={formData.fullName}
+                          onChange={handleChange}
+                          InputProps={{
+                            readOnly: true,
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          fullWidth
+                          id="position"
+                          name="position"
+                          label="Position"
+                          variant="outlined"
+                          value={formData.position}
+                          onChange={handleChange}
+                          InputProps={{
+                            readOnly: true,
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          fullWidth
+                          id="branchID"
+                          name="branchID"
+                          label="Branch ID"
+                          variant="outlined"
+                          value={formData.branchID}
+                          onChange={handleChange}
+                          InputProps={{
+                            readOnly: true,
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          fullWidth
+                          id="branchName"
+                          name="branchName"
+                          label="Branch Name"
+                          variant="outlined"
+                          value={formData.branchName}
+                          onChange={handleChange}
+                          InputProps={{
+                            readOnly: true,
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          fullWidth
+                          id="warehouseCode"
+                          name="warehouseCode"
+                          label="Warehouse Code"
+                          variant="outlined"
+                          value={formData.warehouseCode}
+                          onChange={handleChange}
+                          InputProps={{
+                            readOnly: true,
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          fullWidth
+                          id="priceListNumber"
+                          name="priceListNumber"
+                          label="Price List Number"
+                          variant="outlined"
+                          value={formData.priceListNumber}
+                          onChange={handleChange}
+                          InputProps={{
+                            readOnly: true,
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          fullWidth
+                          id="username"
+                          name="username"
+                          label="Username"
+                          variant="outlined"
+                          value={formData.username}
+                          onChange={handleChange}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <TextField
+                          fullWidth
+                          id="password"
+                          name="password"
+                          label="Password"
+                          type="password"
+                          variant="outlined"
+                          value={formData.password}
+                          onChange={handleChange}
+                        />
+                      </Grid>
+                    </Grid>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      color="primary"
+                      fullWidth
+                      style={{ marginTop: 20 }}
+                    >
+                      Save
+                    </Button>
+                  </form>
+
+                  {/* Show Users Draggable */}
+                  {showUsers && (
+                    <Draggable>
+                      <div
+                        className="bg-white shadow-lg"
+                        style={{
+                          border: "1px solid #ccc",
+                          position: "absolute",
+                          top: "20%",
+                          left: "20%",
+                          maxHeight: "700px",
+                          overflowY: "auto",
+                          background: "white",
+                          zIndex: "9999", // Set a high z-index value to bring it to the front. HAHAHA
+                        }}
+                      >
+                        <div
+                          className="grid grid-cols-2 p-2 text-left windowheader"
+                          style={{ cursor: "move" }}
+                        >
+                          <div>Select User</div>
+                          <div className="text-right">
+                            <span
+                              onClick={handleButtonClick}
+                              className="cursor-pointer"
+                            >
+                              ❌
+                            </span>
+                          </div>
+                        </div>
+                        <div className="content">
+                          <div className="p-2">
+                            <div className="flex items-center">
+                              <div>
+                                Search: <input type="text" className="mb-1" />
+                              </div>
+                              <div className="flex-grow"></div>
+                            </div>
+
+                            <div className="table-container">
+                              <table className="w-full">
+                                <thead className="tables">
+                                  <tr>
+                                    <th>Employee Name</th>
+                                    <th>Position</th>
+                                    <th>Branch ID</th>
+                                    <th>Branch Name</th>
+                                    <th>Designated Warehouse</th>
+                                    <th>Price List Number</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  {employees.map((user, index) => (
+                                    <tr
+                                      key={index}
+                                      onClick={() => handleUserSelect(user)}
+                                      className="hover:bg-blue-100 cursor-pointer"
+                                    >
+                                      <td>{user.EmpName}</td>
+                                      <td>{user.Position}</td>
+                                      <td>{user.BPLId}</td>
+                                      <td>{user.BPLName}</td>
+                                      <td>{user.DflWhs}</td>
+                                      <td>{user.PriceListNum}</td>
+                                    </tr>
+                                  ))}
+                                </tbody>
+                              </table>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </Draggable>
+                  )}
+                  <div className="pt-72"></div>
+                </Container>
+              </div>
             </div>
           </div>
         </Draggable>
