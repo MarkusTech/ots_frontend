c11f53b24a7ee03748ba375dbf72cfc924ad4666
MarkusTech <wm.recopelacion@gmail.com>
2024-05-22 11:38:55 +0800

    Update login.js

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,7 +1,39 @@
 import sqlConn from "../config/db.js";
 
 const login = async (req, res) => {
-  res.send("Login");
+  const { userName, Password } = req.body;
+
+  try {
+    // Query the database for the user
+    const result =
+      await sqlConn.query`SELECT * FROM [dbo].[User] WHERE UserName = ${userName}`;
+    const user = result.recordset[0];
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid username or password" });
+    }
+
+    // Compare the provided password with the password in the database
+    if (Password !== user.Password) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid username or password" });
+    }
+
+    // If credentials are correct
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      userId: user.id,
+      userName: user.UserName,
+      user,
+    });
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
 };
 
 export { login };
