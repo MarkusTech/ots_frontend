98c8e4ce62e22196ec8b12f56d4e65d30a451f98
MarkusTech <wm.recopelacion@gmail.com>
2024-05-29 14:21:55 +0800

    done creating approval type routing

--- backend/controllers/approval/approvalTypeController.js
+++ backend/controllers/approval/approvalTypeController.js
@@ -4,4 +4,26 @@ const saveApprovalType = async (req, res) => {
   res.send("Save Approval");
 };
 
-export { saveApprovalType };
+const getApprovalType = async (req, res) => {
+  res.send("Get all approval type");
+};
+
+const getSingleApprovalType = async (req, res) => {
+  res.send("Get Selected Approval Type");
+};
+
+const updateApprovalType = async (req, res) => {
+  res.send("Update selected Approval Tyope");
+};
+
+const deleteApprovalType = async (req, res) => {
+  res.send("Delete Approval Type");
+};
+
+export {
+  saveApprovalType,
+  getApprovalType,
+  getSingleApprovalType,
+  updateApprovalType,
+  deleteApprovalType,
+};
--- backend/routes/approval/approvalTypeRoutes.js
+++ backend/routes/approval/approvalTypeRoutes.js
@@ -1,7 +1,17 @@
 import express from "express";
-import { saveApprovalType } from "../../controllers/approval/approvalTypeController.js";
+import {
+  saveApprovalType,
+  getApprovalType,
+  getSingleApprovalType,
+  updateApprovalType,
+  deleteApprovalType,
+} from "../../controllers/approval/approvalTypeController.js";
 const router = express.Router();
 
 router.post("/approval/type", saveApprovalType);
+router.get("/approval/type", getApprovalType);
+router.get("/approval/type/:id", getSingleApprovalType);
+router.put("/approval/type/:id", updateApprovalType);
+router.delete("/approval/type", deleteApprovalType);
 
 export default router;
