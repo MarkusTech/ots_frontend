1b58a3cc177bd57e713b6ba47adc7efd1a284e5e
MarkusTech <wm.recopelacion@gmail.com>
2024-05-29 14:33:11 +0800

    Update approvalTypeController.js

--- backend/controllers/approval/approvalTypeController.js
+++ backend/controllers/approval/approvalTypeController.js
@@ -1,7 +1,32 @@
 import sqlConn from "../../config/db.js";
 
 const saveApprovalType = async (req, res) => {
-  res.send("Save Approval");
+  const { AppTypeID, appType } = req.body;
+  try {
+    // Execute the query
+    const result = await sqlConn.query`
+        INSERT INTO [dbo].[AppType]
+               ([AppTypeID], [AppType])
+        VALUES
+               (${AppTypeID}, ${appType});
+      `;
+
+    // Send a success response
+    res.status(200).json({
+      success: true,
+      message: "Data inserted successfully",
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  } finally {
+    // Close the connection
+    sql.close();
+  }
 };
 
 const getApprovalType = async (req, res) => {
