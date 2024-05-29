459116b24cb4d5e6112cd9d9ffded85c9249b79f
MarkusTech <wm.recopelacion@gmail.com>
2024-05-29 13:38:44 +0800

    backend delete unrequired routes and controller

--- backend/controllers/saveDetails.js
+++
@@ -1,7 +0,0 @@
-import sqlConn from "../config/db.js";
-
-const saveDetails = async () => {
-  res.send("Save Details");
-};
-
-export { saveDetails };
--- backend/routes/saveDetailsRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { saveDetails } from "../controllers/saveDetails.js";
-const router = express.Router();
-
-router.post("/details", saveDetails);
-
-export default router;
