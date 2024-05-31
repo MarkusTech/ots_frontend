c16796c9c93f04633ad27e7439e9fc44f9c35d1a
MarkusTech <wm.recopelacion@gmail.com>
2024-05-31 16:23:31 +0800

    Create ReactDraggable.tsx

---
+++ client/src/components/ReactDraggable.tsx
@@ -0,0 +1,23 @@
+// components/DraggableComponent.tsx
+import React, { ReactNode } from "react";
+import Draggable from "react-draggable";
+
+interface DraggableComponentProps {
+  children: ReactNode;
+  initialPosition?: { x: number; y: number };
+}
+
+const DraggableComponent: React.FC<DraggableComponentProps> = ({
+  children,
+  initialPosition = { x: 0, y: 0 },
+}) => {
+  return (
+    <Draggable defaultPosition={initialPosition}>
+      <div className="bg-white shadow-lg" style={{ position: "absolute" }}>
+        {children}
+      </div>
+    </Draggable>
+  );
+};
+
+export default DraggableComponent;
