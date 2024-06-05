6eaf98a874c0efe0db2c8c7cab376a8a23e514ea
MarkusTech <wm.recopelacion@gmail.com>
2024-06-05 11:32:25 +0800

    Create getLastApprovalID.js

---
+++ backend/controllers/approval/getLastApprovalID.js
@@ -0,0 +1,22 @@
+import sqlConn from "../../config/db";
+
+const getLastApprovalID = async (req, res) => {
+  try {
+    const result = await sqlConn.query(`SELECT MAX(AppProcID) AS LastAppProcID
+    FROM [dbo].[AppProc_Main]`);
+
+    res.status(200).json({
+      success: true,
+      message: "Last Approval ID found",
+      data: result.recordset[0],
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { getLastApprovalID };
