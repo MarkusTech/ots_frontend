d1dca671ba14ea70aaaf291fb8bf86022f665c41
MarkusTech <wm.recopelacion@gmail.com>
2024-06-05 11:34:25 +0800

    Update getLastApprovalIDRoutes.js

--- backend/routes/approval/getLastApprovalIDRoutes.js
+++ backend/routes/approval/getLastApprovalIDRoutes.js
@@ -1,2 +1,7 @@
 import express from "express";
 import { getLastApprovalID } from "../../controllers/approval/getLastApprovalID.js";
+const router = express.Router();
+
+router.get("/last-approval-id", getLastApprovalID);
+
+export default router;
