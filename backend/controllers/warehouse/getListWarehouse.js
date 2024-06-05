7d0ec9a4958d73c651b043c53ccabecdbc5afc4e
MarkusTech <wm.recopelacion@gmail.com>
2024-06-05 14:38:02 +0800

    Update getListWarehouse.js

--- backend/controllers/warehouse/getListWarehouse.js
+++ backend/controllers/warehouse/getListWarehouse.js
@@ -1,7 +1,32 @@
 import sqlConn2 from "../../config/db2.js";
 
 const getListOfWarehouse = async (req, res) => {
-  res.send("List");
+  try {
+    const result = await sqlConn2.query(`
+      SELECT [WhsCode], [WhsName]
+      FROM [BCD_TEST_DB].[dbo].[OWHS]
+      WHERE [Inactive] = 'N'
+    `);
+
+    if (!result) {
+      res.status(404).json({
+        success: false,
+        message: "Warehouse not found!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Warehouses fetched!",
+      data: result.recordset,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export { getListOfWarehouse };
