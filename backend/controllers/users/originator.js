3c69c4e14f8c05915246253b73f98d968d781479
MarkusTech <wm.recopelacion@gmail.com>
2024-06-14 13:33:34 +0800

    fix originator controller

--- backend/controllers/users/originator.js
+++ backend/controllers/users/originator.js
@@ -1,7 +1,30 @@
 import sqlConn from "../../config/db.js";
 
 const getOriginator = async (req, res) => {
-  res.send("Originator");
+  try {
+    const result = await sqlConn.query(
+      `Select UserID as UserID, EmpName as EmployeeName, Position as Position from [OTS_DB].[dbo].[User]`
+    );
+
+    if (!result) {
+      res.status(404).json({
+        success: false,
+        message: "Originator List not found!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Originator list fetched successfully",
+      data: result.recordset,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error,
+    });
+  }
 };
 
 export { getOriginator };
