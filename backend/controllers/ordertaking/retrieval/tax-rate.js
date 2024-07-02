d272acc10b8707aa4744a7dc9dbf179c464cc8fa
MarkusTech <wm.recopelacion@gmail.com>
2024-07-02 14:59:04 +0800

    Update tax-rate.js

--- backend/controllers/ordertaking/retrieval/tax-rate.js
+++ backend/controllers/ordertaking/retrieval/tax-rate.js
@@ -1,7 +1,34 @@
 import sqlConn2 from "../../../config/db2.js";
 
 const getTaxRate = async (req, res) => {
-  res.send("tax rate");
+  try {
+    const { taxCode } = req.params;
+
+    const result = await sqlConn2.query(
+      `SELECT Rate FROM [BCD_TEST_DB].[dbo].[OVTG] WHERE Code= '${taxCode}'`
+    );
+
+    if (!result) {
+      res.status(400).json({
+        success: false,
+        message: "Unable to find tax rate",
+      });
+    }
+
+    const data = result.recordset;
+
+    res.status(200).json({
+      success: true,
+      message: "Tax Rate Found",
+      data: data,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export { getTaxRate };
