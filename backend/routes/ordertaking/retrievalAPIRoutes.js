8129429f758a9475a1ace4fbbd778324344ce5a1
MarkusTech <wm.recopelacion@gmail.com>
2024-07-02 14:15:33 +0800

    Update retrievalAPIRoutes.js

--- backend/routes/ordertaking/retrievalAPIRoutes.js
+++ backend/routes/ordertaking/retrievalAPIRoutes.js
@@ -1,7 +1,12 @@
 import express from "express";
 import { getCustomers } from "../../controllers/ordertaking/retrieval/listOfCustomer.js";
+import { getOum } from "../../controllers/ordertaking/retrieval/uom.js";
 const router = express.Router();
 
+// get list of customers
 router.get("/customer", getCustomers);
+
+// get OUM
+router.get("/oum/:itemCode", getOum);
 
 export default router;
