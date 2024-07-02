be150a2659fa20195671b6a437013719e5b13bea
MarkusTech <wm.recopelacion@gmail.com>
2024-07-02 14:55:36 +0800

    Update retrievalAPIRoutes.js

--- backend/routes/ordertaking/retrievalAPIRoutes.js
+++ backend/routes/ordertaking/retrievalAPIRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { getCustomers } from "../../controllers/ordertaking/retrieval/listOfCustomer.js";
 import { getOum } from "../../controllers/ordertaking/retrieval/uom.js";
+import { getTaxRate } from "../../controllers/ordertaking/retrieval/tax-rate.js";
 const router = express.Router();
 
 // get list of customers
@@ -8,5 +9,8 @@ router.get("/customer", getCustomers);
 
 // get OUM
 router.get("/uom/:itemCode", getOum);
+
+// tax-rate
+router.get("/tax-rate/:taxCode", getTaxRate);
 
 export default router;
