2fed9f1731aab25b521e17a1d6aabd494909f4a5
MarkusTech <wm.recopelacion@gmail.com>
2024-07-03 14:35:04 +0800

    Update tax-code.js

--- backend/controllers/ordertaking/retrieval/tax-code.js
+++ backend/controllers/ordertaking/retrieval/tax-code.js
@@ -1,7 +1,32 @@
 import sqlConn2 from "../../../config/db2.js";
 
 const getTaxCode = async (req, res) => {
-  res.send("Tax Code");
+  const { cardCode, warehouseCode } = req.params;
+  try {
+    const result = await sqlConn2.query(
+      `SELECT dbo.fn_GetTaxCode ('${cardCode}','${warehouseCode}') AS TaxCode`
+    );
+
+    if (!result) {
+      res.status(400).json({
+        success: false,
+        message: "Unable to find Tax-Code",
+      });
+    }
+
+    const data = result.recordset;
+    res.status(200).json({
+      success: false,
+      message: "Tax-Code Found",
+      data: data,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export { getTaxCode };
