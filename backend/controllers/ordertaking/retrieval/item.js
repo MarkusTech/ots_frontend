55044d3dfbdc85e99e40e98f2fe25c7ff2c4b926
MarkusTech <wm.recopelacion@gmail.com>
2024-07-03 11:14:16 +0800

    Update item.js

--- backend/controllers/ordertaking/retrieval/item.js
+++ backend/controllers/ordertaking/retrieval/item.js
@@ -1,7 +1,31 @@
 import sqlConn2 from "../../../config/db2.js";
 
 const getItemList = async (req, res) => {
-  res.send("Item List");
+  try {
+    const result = await sqlConn2.query(
+      `SELECT * FROM [BCD_TEST_DB].[dbo].[TVF_ITEM_DETAILS] (14,'GSCNAPGS','C000174') ORDER BY ItemName`
+    );
+
+    if (!result) {
+      res.status(400).json({
+        success: false,
+        message: "Unable to find Items",
+      });
+    }
+
+    const data = result.recordset;
+    res.status(200).json({
+      success: true,
+      message: "Item Feched",
+      data: data,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export { getItemList };
