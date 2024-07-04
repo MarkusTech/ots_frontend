6ddd55b8d12c33bb62efa27106705809fb1c5c47
MarkusTech <wm.recopelacion@gmail.com>
2024-07-04 11:43:52 +0800

    Update sc-discount.js

--- backend/controllers/ordertaking/retrieval/sc-discount.js
+++ backend/controllers/ordertaking/retrieval/sc-discount.js
@@ -1,7 +1,33 @@
 import sqlConn2 from "../../../config/db2.js";
 
 const getScDiscount = async (req, res) => {
-  res.send("Get SC Discount");
+  const {} = req.params;
+  try {
+    const result = await sqlConn2.query(
+      `SELECT [BCD_TEST_DB].dbo.[fn_CheckIfSCPWD] ('${cardCode}','${itemCode}') AS SCDiscount`
+    );
+
+    if (!result) {
+      res.status(400).json({
+        success: false,
+        message: "Unable to find SC-Discount",
+      });
+    }
+
+    const data = result.recordset;
+
+    res.status(200).json({
+      success: true,
+      message: "Sc-discount found",
+      data: data,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export { getScDiscount };
