fe45a05f9a78853087313b3ca5b2b9f8a600010c
MarkusTech <wm.recopelacion@gmail.com>
2024-07-04 16:22:44 +0800

    Update pickup-location.js

--- backend/controllers/ordertaking/retrieval/pickup-location.js
+++ backend/controllers/ordertaking/retrieval/pickup-location.js
@@ -1,7 +1,33 @@
 import sqlConn2 from "../../../config/db2.js";
 
 const pickUpLocation = async (req, res) => {
-  res.send("Pick up Location");
+  const { ItemCode, Qty, Whs } = req.params;
+  try {
+    const result = await sqlConn2.query(
+      `SELECT dbo.fn_GetPickUpLocation('${ItemCode}', ${Qty}, '${Whs}') AS location`
+    );
+
+    if (!result) {
+      res.status(400).json({
+        success: false,
+        message: "Unable to find pickup location",
+      });
+    }
+
+    const data = result.recordset;
+
+    res.status(200).json({
+      success: true,
+      message: "Pick-up location found",
+      data: data,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export { pickUpLocation };
