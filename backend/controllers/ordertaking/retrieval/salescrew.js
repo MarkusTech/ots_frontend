c127e864ab631e2b8a174ec48b628592213b87c4
MarkusTech <wm.recopelacion@gmail.com>
2024-07-05 14:04:50 +0800

    Update salescrew.js

--- backend/controllers/ordertaking/retrieval/salescrew.js
+++ backend/controllers/ordertaking/retrieval/salescrew.js
@@ -2,12 +2,14 @@ import sqlConn2 from "../../../config/db2.js";
 
 const salescrew = async (req, res) => {
   try {
-    const result = await sqlConn2.query(`SELECT DISTINCT T2.SlpName
+    const result = await sqlConn2.query(
+      `SELECT DISTINCT T2.SlpName
       FROM [BCD_TEST_DB].dbo.OUSR T0
       INNER JOIN [BCD_TEST_DB].dbo.OUDG T1 ON T0.DfltsGroup = T1.Code
       INNER JOIN [BCD_TEST_DB].dbo.OSLP T2 ON T2.Memo = T1.Warehouse
       WHERE T0.INTERNAL_K =  29
-      ORDER BY T2.SLPNAME ASC`);
+      ORDER BY T2.SLPNAME ASC`
+    );
 
     if (!result) {
       res.status(400).json({
