d48a796782323eaded7e009d4bec3c6bd5e5f511
MarkusTech <wm.recopelacion@gmail.com>
2024-07-05 10:58:34 +0800

    Update warehouse-soh.js

--- backend/controllers/ordertaking/retrieval/warehouse-soh.js
+++ backend/controllers/ordertaking/retrieval/warehouse-soh.js
@@ -1,7 +1,31 @@
 import sqlConn2 from "../../../config/db2.js";
 
 const warehouseSoh = async (req, res) => {
-  res.send("warehouse");
+  try {
+    const result = await sqlConn2.query(
+      `SELECT * FROM dbo.TVF_WHSE_SOH ('${itemCode}','${UoM}',${branchID})`
+    );
+
+    if (!result) {
+      res.status(400).json({
+        success: false,
+        message: "Unable to find Warehouse Soh",
+      });
+    }
+
+    const data = result.recordset;
+    res.status(200).json({
+      success: true,
+      message: "Warehouse Soh Fetched",
+      data: data,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export { warehouseSoh };
