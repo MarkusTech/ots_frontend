cd78d8e82f47093c23828440ad7f48f29d6e587b
MarkusTech <wm.recopelacion@gmail.com>
2024-07-25 14:53:52 +0800

    Update db2.js

--- backend/config/db2.js
+++ backend/config/db2.js
@@ -5,15 +5,18 @@ dotenv.config();
 
 const config = {
   database: process.env.MSSQL_DATABASE_MAIN,
-  server: process.env.MSSQL_SERVER_MAIN,
+  server: process.env.MSSQL_SERVER_MAIN, // Use the hostname or domain name here
   user: process.env.MSSQL_USERNAME_MAIN,
   password: process.env.MSSQL_PASSWORD_MAIN,
   options: {
-    encrypt: true,
-    trustServerCertificate: true, // Change to false if you have a valid certificate
+    trustedConnection: true,
+    trustServerCertificate: true,
+    cryptoCredentialsDetails: {
+      minVersion: "TLSv1",
+      servername: process.env.MSSQL_SERVER_MAIN, // Use the hostname or domain name here
+    },
   },
 };
-
 const sqlConn2 = sql.connect(config, (err) => {
   if (err) console.log(err);
   console.log("MSSQL2 Database Connected".bgGreen);
