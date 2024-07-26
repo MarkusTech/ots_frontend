cfe56a1cf9e6277bed4138c500fbf81569df2d96
MarkusTech <wm.recopelacion@gmail.com>
2024-07-26 15:04:58 +0800

    Update retrievalController.js

--- backend_retrieval/controller/retrievalController.js
+++ backend_retrieval/controller/retrievalController.js
@@ -308,7 +308,45 @@ class RetrievalController {
       });
     }
   }
-  //
+
+  // srp
+  static async srp(req, res) {
+    const {
+      itemCode,
+      ItemsPerUnit,
+      UoM,
+      taxCode,
+      lowerbound,
+      vendorCode,
+      PriceListNum,
+    } = req.params;
+    try {
+      const result = await sqlConn2.query(
+        `SELECT [BCD_TEST_DB].dbo.fn_GetSRP ('${itemCode}', ${ItemsPerUnit}, '${UoM}','${taxCode}', ${lowerbound}, '${vendorCode}',${PriceListNum}) AS SRP`
+      );
+
+      if (!result) {
+        res.status(400).json({
+          success: false,
+          message: "Unable to find SRP",
+        });
+      }
+
+      const data = result.recordset;
+
+      res.status(200).json({
+        success: true,
+        message: "SRP found",
+        data: data,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  }
   //
   //
   //
