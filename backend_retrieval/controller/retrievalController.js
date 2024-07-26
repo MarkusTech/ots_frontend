c822b2e20f505cf39c41f12a3fa79b698fa91a15
MarkusTech <wm.recopelacion@gmail.com>
2024-07-26 15:08:42 +0800

    Update retrievalController.js

--- backend_retrieval/controller/retrievalController.js
+++ backend_retrieval/controller/retrievalController.js
@@ -471,6 +471,36 @@ class RetrievalController {
       });
     }
   }
+
+  // warehouse-soh
+  static async warehouseSoh(req, res) {
+    const { itemCode, UoM, branchID } = req.params;
+    try {
+      const result = await sqlConn2.query(
+        `SELECT * FROM [BCD_TEST_DB].dbo.TVF_WHSE_SOH ('${itemCode}','${UoM}',${branchID})`
+      );
+
+      if (!result) {
+        res.status(400).json({
+          success: false,
+          message: "Unable to find Warehouse Soh",
+        });
+      }
+
+      const data = result.recordset;
+      res.status(200).json({
+        success: true,
+        message: "Warehouse Soh Fetched",
+        data: data,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  }
 }
 
 export default RetrievalController;
