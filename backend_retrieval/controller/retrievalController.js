b1f086971fde5d7589ee52d4701f3f2394a6631a
MarkusTech <wm.recopelacion@gmail.com>
2024-07-26 15:04:22 +0800

    Update retrievalController.js

--- backend_retrieval/controller/retrievalController.js
+++ backend_retrieval/controller/retrievalController.js
@@ -278,7 +278,36 @@ class RetrievalController {
     }
   }
 
-  //
+  // sc-discount
+  static async getScDiscount(req, res) {
+    const { cardCode, itemCode } = req.params;
+    try {
+      const result = await sqlConn2.query(
+        `SELECT [BCD_TEST_DB].dbo.[fn_CheckIfSCPWD] ('${cardCode}','${itemCode}') AS SCDiscount`
+      );
+
+      if (!result) {
+        res.status(400).json({
+          success: false,
+          message: "Unable to find SC-Discount",
+        });
+      }
+
+      const data = result.recordset;
+
+      res.status(200).json({
+        success: true,
+        message: "Sc-discount found",
+        data: data,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  }
   //
   //
   //
