a1a740ad2204a3cad5780f0b19935d1e825916e4
MarkusTech <wm.recopelacion@gmail.com>
2024-07-26 15:02:47 +0800

    Update retrievalController.js

--- backend_retrieval/controller/retrievalController.js
+++ backend_retrieval/controller/retrievalController.js
@@ -213,7 +213,36 @@ class RetrievalController {
     }
   }
 
-  //
+  // pick up location
+  static async pickUpLocation(req, res) {
+    const { ItemCode, Qty, Whs } = req.params;
+    try {
+      const result = await sqlConn2.query(
+        `SELECT [BCD_TEST_DB].dbo.fn_GetPickUpLocation('${ItemCode}', ${Qty}, '${Whs}') AS location`
+      );
+
+      if (!result) {
+        res.status(400).json({
+          success: false,
+          message: "Unable to find pickup location",
+        });
+      }
+
+      const data = result.recordset;
+
+      res.status(200).json({
+        success: true,
+        message: "Pick-up location found",
+        data: data,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  }
   //
   //
 }
