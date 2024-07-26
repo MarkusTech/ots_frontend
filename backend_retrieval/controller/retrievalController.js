dd9fc5c5052cd41679f648dbdd7c5b4c0c4ffa15
MarkusTech <wm.recopelacion@gmail.com>
2024-07-26 15:07:24 +0800

    Update retrievalController.js

--- backend_retrieval/controller/retrievalController.js
+++ backend_retrieval/controller/retrievalController.js
@@ -409,6 +409,38 @@ class RetrievalController {
     }
   }
 
+  // tax-rate
+  static async getTaxRate(req, res) {
+    try {
+      const { taxCode } = req.params;
+
+      const result = await sqlConn2.query(
+        `SELECT Rate FROM [BCD_TEST_DB].[dbo].[OVTG] WHERE Code= '${taxCode}'`
+      );
+
+      if (!result) {
+        res.status(400).json({
+          success: false,
+          message: "Unable to find tax rate",
+        });
+      }
+
+      const data = result.recordset;
+
+      res.status(200).json({
+        success: true,
+        message: "Tax Rate Found",
+        data: data,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  }
+
   //
 }
 
