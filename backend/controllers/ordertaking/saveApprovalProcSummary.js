7d8b956dd53db4ab0c785eae27b23506a9d99465
MarkusTech <wm.recopelacion@gmail.com>
2024-08-06 11:22:32 +0800

    Update saveApprovalProcSummary.js

--- backend/controllers/ordertaking/saveApprovalProcSummary.js
+++ backend/controllers/ordertaking/saveApprovalProcSummary.js
@@ -1,7 +1,22 @@
 import sqlConn from "../../config/db.js";
 
 const saveApprovalSummary = async (req, res) => {
-  req.send("Approval Procedure Summary");
+  const {
+    AppProcID,
+    ReqDate,
+    DocType,
+    DraftNum,
+    Approver,
+    Originator,
+    Remarks,
+    Status,
+  } = req.body;
+
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export { saveApprovalSummary };
