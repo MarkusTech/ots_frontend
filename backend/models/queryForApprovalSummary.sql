ed48ed8034a82d8378d725440ee51edd5cac92b1
MarkusTech <wm.recopelacion@gmail.com>
2024-08-16 14:09:05 +0800

    Create queryForApprovalSummary.sql

---
+++ backend/models/queryForApprovalSummary.sql
@@ -0,0 +1,41 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [AppID]
+      ,[AppProcID]
+      ,[UserID]
+      ,[AppLevel]
+  FROM [OTS_DB].[dbo].[AppProc_DetApp]
+
+
+  select * from [OTS_DB].[dbo].[AppProc_DetApp]
+
+  select * from [OTS_DB].[dbo].[AppProc_DetOrig]
+
+  select * from [OTS_DB].[dbo].[AppProc_Main]
+
+  select * from [OTS_DB].[dbo].[AppProc_Summary]
+
+  select AppTypeID from [OTS_DB].[dbo].[AppType]
+
+  select AppTypeID from [OTS_DB].[dbo].[AppType] where  AppType like '%Below Standard Discounting%'
+
+
+  DELETE FROM [OTS_DB].[dbo].[AppProc_DetApp]
+	WHERE AppProcID NOT IN (1038, 1039, 1040, 1041, 1042);
+
+  SELECT m.*
+	FROM [OTS_DB].[dbo].[AppProc_Main] m
+	INNER JOIN [OTS_DB].[dbo].[AppType] t
+    ON m.AppTypeID = t.AppTypeID
+	WHERE t.AppType LIKE '%Below Standard Discounting%';
+
+  
+  select AppTypeID from [OTS_DB].[dbo].[AppType] where  AppType like '%Below Standard Discounting%'
+
+  SELECT m.AppProcID
+	FROM [OTS_DB].[dbo].[AppProc_Main] m
+	INNER JOIN [OTS_DB].[dbo].[AppType] t
+		ON m.AppTypeID = t.AppTypeID
+	WHERE t.AppTypeID = 1123123126;
+
+  SELECT * from [OTS_DB].[dbo].[AppProc_DetOrig] Where AppProcID = 1038
+  SELECT * from [OTS_DB].[dbo].[AppProc_DetApp] Where AppProcID = 1038
