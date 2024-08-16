6d6336db3488ab161ebb80b137674c37e67eefc2
MarkusTech <wm.recopelacion@gmail.com>
2024-08-16 14:59:12 +0800

    Update getApprovalProcedureSummaryRoutes.js

--- backend/routes/approval/getApprovalProcedureSummaryRoutes.js
+++ backend/routes/approval/getApprovalProcedureSummaryRoutes.js
@@ -2,6 +2,6 @@ import express from "express";
 import { getBelowStandarDiscounting } from "../../controllers/approval/getApprovalProcedureSummary.js";
 const router = express.Router();
 
-router.get("/", getBelowStandarDiscounting);
+router.get("/get-below-standard-discounting", getBelowStandarDiscounting);
 
 export default router;
