bd687852a42c9e4120fb7536305d2e45095a8368
MarkusTech <wm.recopelacion@gmail.com>
2024-09-03 14:00:12 +0800

    update for notification count

--- client/src/components/NotificationBell.tsx
+++ client/src/components/NotificationBell.tsx
@@ -1,17 +1,27 @@
 // components/NotificationBell.tsx
-import React, { useState, useEffect } from "react";
-import styles from "./NotificationBell.module.css";
+import React from "react";
+import Badge from "@mui/material/Badge";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import { styled } from "@mui/material/styles";
 
 type NotificationBellProps = {
   unseenCount: number;
 };
 
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: "0 4px",
+  },
+}));
+
 const NotificationBell: React.FC<NotificationBellProps> = ({ unseenCount }) => {
   return (
-    <div className={styles.notificationBell}>
-      <i className="fa fa-bell"></i>
-      {unseenCount > 0 && <span className={styles.badge}>{unseenCount}</span>}
-    </div>
+    <StyledBadge badgeContent={unseenCount} color="error">
+      <NotificationsIcon fontSize="large" />
+    </StyledBadge>
   );
 };
 
