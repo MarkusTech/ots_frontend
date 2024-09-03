048d8f819853b890681ffa13697ec0c733ee6d77
MarkusTech <wm.recopelacion@gmail.com>
2024-09-03 11:05:50 +0800

    Update approvalNotification.js

--- backend/controllers/approval/approvalNotification.js
+++ backend/controllers/approval/approvalNotification.js
@@ -3,14 +3,14 @@ import sqlConn from "../../config/db.js";
 const approvalNotification = async (req, res) => {
   try {
     const result = await sqlConn.query(
-      `select count(AppProcID) from [OTS_DB].[dbo].[AppProc_Summary] Where status = 'pending'`
+      `select count(AppProcID) as approverCount from [OTS_DB].[dbo].[AppProc_Summary] Where status = 'pending'`
     );
 
-    const notificationCount = result.recordset[0];
+    const notificationCount = result.recordset[0].approverCount;
 
     res.status(200).json({
       success: true,
-      approvalCount: notificationCount,
+      approverCount: notificationCount,
     });
   } catch (error) {
     console.log(error);
