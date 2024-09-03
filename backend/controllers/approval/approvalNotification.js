c34718b8c9c59c71f8c407ef62313d1e8d041925
MarkusTech <wm.recopelacion@gmail.com>
2024-09-03 10:46:17 +0800

    Update approvalNotification.js

--- backend/controllers/approval/approvalNotification.js
+++ backend/controllers/approval/approvalNotification.js
@@ -1,7 +1,22 @@
 import sqlConn from "../../config/db.js";
 
 const approvalNotification = async (req, res) => {
-  res.send("Approval Notification");
+  try {
+    const result = await sqlConn.query(
+      `select count(AppProcID) from [OTS_DB].[dbo].[AppProc_Summary] Where AppProcID = 1038`
+    );
+
+    res.status(200).json({
+      success: true,
+      data: result.recordset[0],
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export { approvalNotification };
