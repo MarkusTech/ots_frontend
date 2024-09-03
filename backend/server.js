12e746e10fc2f0380f88960e0f68264a3e5679fb
MarkusTech <wm.recopelacion@gmail.com>
2024-09-03 10:40:48 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ import saveDetailsRoutes from "./routes/ordertaking/saveDetails.js";
 import salesCrewRoutes from "./routes/ordertaking/getSalesCrewRoutes.js";
 import saveApprovalSummaryRoutes from "./routes/ordertaking/saveAppProcSummaryRoutes.js";
 import getApprovalProcedureSummaryRoutes from "./routes/approval/getApprovalProcedureSummaryRoutes.js";
+import approvalNotificationRoutes from "./routes/approval/approvalNotificationRoutes.js";
 
 // Routes v2
 import db2_paylocRoutes from "./routes/location/db2_paylocRoutes.js";
@@ -76,6 +77,7 @@ app.use("/api/v1", saveDetailsRoutes);
 app.use("/api/v1", salesCrewRoutes);
 app.use("/api/v1", saveApprovalSummaryRoutes);
 app.use("/api/v1", getApprovalProcedureSummaryRoutes);
+app.use("/api/v1", approvalNotificationRoutes);
 
 // API V2
 app.use("/api/v2", db2_paylocRoutes);
