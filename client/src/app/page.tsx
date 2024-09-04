ae4d2abe8ef7209ecebdbeaf56499881bad518d8
MarkusTech <wm.recopelacion@gmail.com>
2024-09-04 10:02:04 +0800

    Update page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Draggable from "react-draggable";
 import SalesOrder from "./Transaction/SalesOrder/SalesOrder";
 import ViewUser from "./Setup/ViewUsers/page";
