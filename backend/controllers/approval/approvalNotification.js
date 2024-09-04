a4712e13f2a1404284e862cde4ae827b197b7815
MarkusTech <wm.recopelacion@gmail.com>
2024-09-04 13:53:10 +0800

    Update approvalNotification.js

--- backend/controllers/approval/approvalNotification.js
+++ backend/controllers/approval/approvalNotification.js
@@ -2,21 +2,22 @@ import sqlConn from "../../config/db.js";
 
 const approvalNotification = async (req, res) => {
   try {
-    const result = await sqlConn.query(
-      `select count(AppProcID) as approverCount from [OTS_DB].[dbo].[AppProc_Summary] Where status = 'pending'`
-    );
-
-    const notificationCount = result.recordset[0].approverCount;
+    const query = `
+      SELECT COUNT(AppProcID) AS approverCount 
+      FROM [OTS_DB].[dbo].[AppProc_Summary] 
+      WHERE status = 'pending'
+    `;
+    const { recordset } = await sqlConn.query(query);
 
     res.status(200).json({
       success: true,
-      approverCount: notificationCount,
+      approverCount: recordset[0]?.approverCount || 0,
     });
   } catch (error) {
-    console.log(error);
+    console.error("Error fetching approver count:", error);
     res.status(500).json({
       success: false,
-      message: error.message,
+      message: "Internal Server Error",
     });
   }
 };
