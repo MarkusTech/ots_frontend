495ef03b6a853696a3548cffccff03d1f9e52636
MarkusTech <wm.recopelacion@gmail.com>
2024-09-06 11:46:56 +0800

    Update getSingleDraftData.js

--- backend/controllers/ordertaking/getSingleDraftData.js
+++ backend/controllers/ordertaking/getSingleDraftData.js
@@ -1,15 +1,5 @@
 import sqlConn from "../../config/db.js";
 
-// const getDataFromDraft = async (req, res) => {
-//   try {
-//     const data = await sqlConn.query("Select * From SO_Header");
-//     res.status(200).json(data.recordset);
-//   } catch (error) {
-//     console.log(error);
-//     throw new Error(error);
-//   }
-// };
-
 const getDataFromDraft = async (req, res) => {
   try {
     const data = await sqlConn.query(
