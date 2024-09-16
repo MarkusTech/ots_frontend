aca7d41254f304064462382ca1e2ce9b987a257e
MarkusTech <wm.recopelacion@gmail.com>
2024-09-16 15:48:40 +0800

    Update NotificationList.tsx

--- client/src/app/Transaction/SalesOrder/NotificationList.tsx
+++ client/src/app/Transaction/SalesOrder/NotificationList.tsx
@@ -22,6 +22,7 @@ const NotificationList = () => {
   const [notifications, setNotifications] = useState<NotificationArr[]>([]);
   const [status, setStatus] = useState<string>("");
   const [showSalesOrder, setShowSalesOrder] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
     const fetchNotifications = async () => {
@@ -98,102 +99,118 @@ const NotificationList = () => {
     setShowSalesOrder(true);
   };
 
-  return (
-    <Draggable>
-      <div
-        className="bg-white shadow-lg"
-        style={{
-          border: "1px solid #ccc",
-          position: "absolute",
-          top: "20%",
-          left: "20%",
-          maxHeight: "700px",
-          overflowY: "auto",
-        }}
-      >
-        <div
-          className="grid grid-cols-2 p-2 text-left windowheader"
-          style={{ cursor: "move" }}
-        >
-          <div>Approval List</div>
-          <div className="text-right">
-            <span className="cursor-pointer">❌</span>
-          </div>
-        </div>
-        <div className="content">
-          <div className="p-2">
-            <div className="table-container">
-              <table className="w-full">
-                <thead className="tables">
-                  <tr>
-                    <th>Approval Summary ID</th>
-                    <th>Approval Type</th>
-                    <th>Request Date</th>
-                    <th>Draft Number</th>
-                    <th>Document Date</th>
-                    <th>Document Type</th>
-                    <th>Customer Name</th>
-                    <th>Total Amount Due</th>
-                    <th>Remarks</th>
-                    <th>Status</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {notifications.map((notification) => (
-                    <tr key={notification.AppSummID}>
-                      <td>{notification.AppSummID}</td>
-                      <td>{notification.AppType}</td>
-                      <td>
-                        {new Date(notification.ReqDate).toLocaleDateString()}
-                      </td>
-                      <td>{notification.DraftNum}</td>
-                      <td>
-                        {new Date(notification.DocDate).toLocaleDateString()}
-                      </td>
-                      <td>{notification.DocType}</td>
-                      <td>{notification.CustomerName}</td>
-                      <td>{notification.TotalAmtDue}</td>
-                      <td>{notification.Remarks}</td>
-                      <td>
-                        <select
-                          value={notification.Status}
-                          onChange={(e) =>
-                            handleStatusChange(e, notification.AppSummID)
-                          }
-                          className="text-blue-500"
-                        >
-                          <option value="pending">Pending</option>
-                          <option value="approve">Approve</option>
-                          <option value="decline">Reject</option>
-                        </select>
-                      </td>
-                      <td>
-                        <button
-                          className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600"
-                          onClick={() => handleUpdate(notification.AppSummID)}
-                        >
-                          Update
-                        </button>
+  const handleClose = () => {
+    setIsVisible(false); // Hide the draggable component
+  };
 
-                        <button
-                          className="ml-4 bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600"
-                          onClick={handleShowView}
-                        >
-                          View
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-                {/* Conditionally render SalesOrder */}
-                {/* {showSalesOrder && <SalesOrder />} */}
-              </table>
+  return (
+    <>
+      {isVisible && (
+        <Draggable>
+          <div
+            className="bg-white shadow-lg"
+            style={{
+              border: "1px solid #ccc",
+              position: "absolute",
+              top: "20%",
+              left: "20%",
+              maxHeight: "700px",
+              overflowY: "auto",
+            }}
+          >
+            <div
+              className="grid grid-cols-2 p-2 text-left windowheader"
+              style={{ cursor: "move" }}
+            >
+              <div>Approval List</div>
+              <div className="text-right">
+                <span className="cursor-pointer" onClick={handleClose}>
+                  ❌
+                </span>
+              </div>
+            </div>
+            <div className="content">
+              <div className="p-2">
+                <div className="table-container">
+                  <table className="w-full">
+                    <thead className="tables">
+                      <tr>
+                        <th>Approval Summary ID</th>
+                        <th>Approval Type</th>
+                        <th>Request Date</th>
+                        <th>Draft Number</th>
+                        <th>Document Date</th>
+                        <th>Document Type</th>
+                        <th>Customer Name</th>
+                        <th>Total Amount Due</th>
+                        <th>Remarks</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {notifications.map((notification) => (
+                        <tr key={notification.AppSummID}>
+                          <td>{notification.AppSummID}</td>
+                          <td>{notification.AppType}</td>
+                          <td>
+                            {new Date(
+                              notification.ReqDate
+                            ).toLocaleDateString()}
+                          </td>
+                          <td>{notification.DraftNum}</td>
+                          <td>
+                            {new Date(
+                              notification.DocDate
+                            ).toLocaleDateString()}
+                          </td>
+                          <td>{notification.DocType}</td>
+                          <td>{notification.CustomerName}</td>
+                          <td>{notification.TotalAmtDue}</td>
+                          <td>{notification.Remarks}</td>
+                          <td>
+                            <select
+                              value={notification.Status}
+                              onChange={(e) =>
+                                handleStatusChange(e, notification.AppSummID)
+                              }
+                              className="text-blue-500"
+                            >
+                              <option value="pending">Pending</option>
+                              <option value="approve">Approve</option>
+                              <option value="decline">Reject</option>
+                            </select>
+                          </td>
+                          <td>
+                            <button
+                              className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600"
+                              onClick={() =>
+                                handleUpdate(notification.AppSummID)
+                              }
+                            >
+                              Update
+                            </button>
+
+                            <button
+                              className="ml-4 bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600"
+                              onClick={handleShowView}
+                            >
+                              View
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                    {/* Conditionally render SalesOrder */}
+                    {/* {showSalesOrder && <SalesOrder />} */}
+                  </table>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </Draggable>
+        </Draggable>
+      )}
+    </>
   );
 };
 
