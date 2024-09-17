86933bbb7c7cb34d7b22b6a21732217efa7e3957
MarkusTech <wm.recopelacion@gmail.com>
2024-09-17 13:54:30 +0800

    Update appProcSummaryRoutes.js

--- backend/routes/ordertaking/appProcSummaryRoutes.js
+++ backend/routes/ordertaking/appProcSummaryRoutes.js
@@ -3,11 +3,17 @@ import {
   saveApprovalSummary,
   getApprovalProcedureSummary,
   updateApprovalSummaryStatus,
+  getSalesOrderBasedOnApprovalDraftNum,
 } from "../../controllers/ordertaking/approvalProcSummary.js";
 const router = express.Router();
 
 router.post("/approval-summary", saveApprovalSummary);
 router.get("/approval-summary", getApprovalProcedureSummary);
 router.put("/approval-summary/:AppSummID", updateApprovalSummaryStatus);
+// to get the data for sales order
+router.get(
+  "/approval-summary/sales-order/:DraftNum",
+  getSalesOrderBasedOnApprovalDraftNum
+);
 
 export default router;
