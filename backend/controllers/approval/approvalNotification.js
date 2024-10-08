c13bcccb6e18d7c8efacc42296eff6d1ef50d493
MarkusTech <wm.recopelacion@gmail.com>
2024-10-08 10:40:11 +0800

    Update approvalNotification.js

--- backend/controllers/approval/approvalNotification.js
+++ backend/controllers/approval/approvalNotification.js
@@ -40,6 +40,7 @@ const approvalNotification = async (req, res) => {
 
 const orignatorNotificationCount = async (req, res) => {
   try {
+    const query = ``;
   } catch (error) {
     console.error("Error fetching approver count:", error);
     res.status(500).json({
@@ -51,6 +52,47 @@ const orignatorNotificationCount = async (req, res) => {
 
 const originatorList = async (req, res) => {
   try {
+    const query = `WITH SummaryData AS (
+                    SELECT 
+                        MIN(APS.AppSummID) AS AppSummID,
+                        AT.AppType,         
+                        MAX(APS.ReqDate) AS ReqDate,          
+                        APS.DraftNum,   
+                        MAX(SH.DocDate) AS DocDate,
+                        APS.DocType,
+                        MAX(SH.CustomerName) AS CustomerName,
+                        MAX(SH.TotalAmtDue) AS TotalAmtDue,
+                        MIN(APS.Remarks) AS Remarks,
+                        MAX(APS.Status) AS Status
+                    FROM 
+                        [OTS_DB].[dbo].[AppProc_Summary] APS
+                    INNER JOIN 
+                        [OTS_DB].[dbo].[AppProc_Main] AM ON APS.AppProcID = AM.AppProcID
+                    INNER JOIN 
+                        [OTS_DB].[dbo].[AppType] AT ON AT.AppTypeID = AM.AppTypeID
+                    INNER JOIN 
+                        [OTS_DB].[dbo].[SO_Header] SH ON APS.DraftNum = SH.DraftNum
+                    GROUP BY 
+                        AT.AppType,
+                        APS.DraftNum,
+                        APS.DocType
+                )
+
+                SELECT 
+                    *,
+                    (SELECT COUNT(*) FROM SummaryData) AS TotalRecordCount  -- Count of all output rows
+                FROM 
+                    SummaryData
+                ORDER BY 
+                    DraftNum;`;
+
+    const data = query.recordset;
+
+    res.status(200).json({
+      success: true,
+      message: "Approver List",
+      data: data,
+    });
   } catch (error) {
     console.error("Error fetching approver count:", error);
     res.status(500).json({
