2da2fca7c1cafdc1c064016797e41518dd2a4545
MarkusTech <wm.recopelacion@gmail.com>
2024-10-09 13:50:50 +0800

    Update Approver List

--- backend/controllers/approval/approvalNotification.js
+++ backend/controllers/approval/approvalNotification.js
@@ -38,6 +38,44 @@ const approvalNotification = async (req, res) => {
   }
 };
 
+const approverList = async (req, res) => {
+  try {
+    const { approverID } = req.params;
+    const result = `SELECT APS.AppSummID,APS.Approver ,AT.AppType, APS.ReqDate, APS.DraftNum, SH.DocDate, APS.DocType, SH.CustomerName, SH.TotalAmtDue, APS.Remarks, APS.Status
+        from [OTS_DB].[dbo].[AppProc_Summary] APS
+        INNER JOIN [OTS_DB].[dbo].[AppProc_Main] AM
+        ON APS.AppProcID = AM.AppProcID
+        INNER JOIN [OTS_DB].[dbo].[AppType] AT
+        ON AT.AppTypeID = AM.AppTypeID
+        INNER JOIN [OTS_DB].[dbo].[SO_Header] SH
+        ON APS.DraftNum = SH.DraftNum WHERE APS.Approver = ${approverID} Order by Aps.AppSummID Desc`;
+
+    if (!result.recordset.length) {
+      return res.status(404).json({
+        success: false,
+        message: "Unable to find List of Approval Summary",
+      });
+    }
+
+    const data = result.recordset;
+
+    // Store the fetched data in the cache
+    cache.set(cacheKey, data);
+
+    res.status(200).json({
+      success: true,
+      data: data,
+      message: "Approval Procedure Summary fetched successfully",
+    });
+  } catch (error) {
+    console.error("Error fetching approver list:", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
 const orignatorNotificationCount = async (req, res) => {
   try {
     const cacheKey = "orignatorNotification:totalRecordCount";
@@ -162,4 +200,9 @@ const originatorList = async (req, res) => {
   }
 };
 
-export { approvalNotification, orignatorNotificationCount, originatorList };
+export {
+  approvalNotification,
+  orignatorNotificationCount,
+  originatorList,
+  approverList,
+};
--- backend/routes/approval/approvalNotificationRoutes.js
+++ backend/routes/approval/approvalNotificationRoutes.js
@@ -3,11 +3,14 @@ import {
   approvalNotification,
   originatorList,
   orignatorNotificationCount,
+  approverList,
 } from "../../controllers/approval/approvalNotification.js";
 const router = express.Router();
 
 router.get("/approval-notification", approvalNotification);
 router.get("/originator-list", originatorList);
 router.get("/originator-notification", orignatorNotificationCount);
+
+router.get("/approver-list/:approverID", approverList);
 
 export default router;
