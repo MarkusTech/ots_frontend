a3347b3a55136ac3f8fcb6ad11b8ecfc818421e6
MarkusTech <wm.recopelacion@gmail.com>
2024-10-09 10:13:24 +0800

    Update OriginatorNotificationList.tsx

--- client/src/app/Transaction/SalesOrder/OriginatorNotificationList.tsx
+++ client/src/app/Transaction/SalesOrder/OriginatorNotificationList.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Draggable from "react-draggable";
+import Swal from "sweetalert2";
 
 const OriginatorNotificationList = () => {
   return <div>OriginatorNotificationList</div>;
