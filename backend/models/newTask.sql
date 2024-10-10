726d9cf812387e9fdc5c735ddc31df85db7e2413
MarkusTech <wm.recopelacion@gmail.com>
2024-10-10 16:33:01 +0800

    Update newTask.sql

--- backend/models/newTask.sql
+++ backend/models/newTask.sql
@@ -17,6 +17,8 @@ select AppProcID from [OTS_DB].[dbo].[AppProc_Summary] Where Approver = 71 and S
 
 select AppLevel, UserID from [OTS_DB].[dbo].[AppProc_DetApp] Where AppProcID = 1038
 
+select AppProcID, Type FROM [OTS_DB].[dbo].[AppProc_Main]
+
 
 
 SELECT 
@@ -31,3 +33,43 @@ WHERE
     APS.Approver = 71
     AND APS.Status = 'pending'
     AND APS.AppProcID = 1038;
+
+SELECT 
+    APS.AppProcID, 
+    APD.AppLevel, 
+    APD.UserID,
+    APM.Type
+FROM 
+    [OTS_DB].[dbo].[AppProc_Summary] APS
+INNER JOIN 
+    [OTS_DB].[dbo].[AppProc_DetApp] APD ON APS.AppProcID = APD.AppProcID
+INNER JOIN 
+    [OTS_DB].[dbo].[AppProc_Main] APM ON APS.AppProcID = APM.AppProcID
+WHERE 
+    APS.Approver = 71
+    AND APS.Status = 'pending'
+    AND APS.AppProcID = 1038;
+
+
+SELECT 
+    APS.AppProcID, 
+    MAX(APD.AppLevel) AS MaxAppLevel,  -- Get the maximum AppLevel per AppProcID
+    APD.UserID,
+    APM.Type
+FROM 
+    [OTS_DB].[dbo].[AppProc_Summary] APS
+INNER JOIN 
+    [OTS_DB].[dbo].[AppProc_DetApp] APD ON APS.AppProcID = APD.AppProcID
+INNER JOIN 
+    [OTS_DB].[dbo].[AppProc_Main] APM ON APS.AppProcID = APM.AppProcID
+WHERE 
+    APS.Approver = 71
+    AND APS.Status = 'pending'
+GROUP BY 
+    APS.AppProcID, 
+    APD.UserID,
+    APM.Type
+ORDER BY 
+    APS.AppProcID;
+
+
