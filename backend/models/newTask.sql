0658bbddc54f37021116806398c34d0a0d52ea72
MarkusTech <wm.recopelacion@gmail.com>
2024-10-10 15:22:48 +0800

    Create newTask.sql

---
+++ backend/models/newTask.sql
@@ -0,0 +1,33 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT * FROM [OTS_DB].[dbo].[AppProc_Summary]
+
+SELECT * FROM [OTS_DB].[dbo].[AppProc_Main]
+
+SELECT * FROM [OTS_DB].[dbo].[AppProc_DetApp]
+
+SELECT APS.AppSummID, APS.Approver, AT.AppType, APS.ReqDate, APS.DraftNum, SH.DocDate, APS.DocType, SH.CustomerName, SH.TotalAmtDue, APS.Remarks, APS.Status
+      FROM [OTS_DB].[dbo].[AppProc_Summary] APS
+      INNER JOIN [OTS_DB].[dbo].[AppProc_Main] AM ON APS.AppProcID = AM.AppProcID
+      INNER JOIN [OTS_DB].[dbo].[AppType] AT ON AT.AppTypeID = AM.AppTypeID
+      INNER JOIN [OTS_DB].[dbo].[SO_Header] SH ON APS.DraftNum = SH.DraftNum
+      WHERE APS.Approver = 71
+      ORDER BY APS.AppSummID DESC
+
+select AppProcID from [OTS_DB].[dbo].[AppProc_Summary] Where Approver = 71 and Status = 'pending'
+
+select AppLevel, UserID from [OTS_DB].[dbo].[AppProc_DetApp] Where AppProcID = 1038
+
+
+
+SELECT 
+    APS.AppProcID,
+    APD.AppLevel,
+    APD.UserID
+FROM 
+    [OTS_DB].[dbo].[AppProc_Summary] APS
+INNER JOIN 
+    [OTS_DB].[dbo].[AppProc_DetApp] APD ON APS.AppProcID = APD.AppProcID
+WHERE 
+    APS.Approver = 71
+    AND APS.Status = 'pending'
+    AND APS.AppProcID = 1038;
