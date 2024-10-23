b38498513917e0dbb64c0ffa578fe993795b20b7
MarkusTech <wm.recopelacion@gmail.com>
2024-10-23 11:36:46 +0800

    Update newTaskV2.sql

--- backend/models/newTaskV2.sql
+++ backend/models/newTaskV2.sql
@@ -28,3 +28,46 @@ WHERE APS.Approver = 71
     OR AM.Type != 'sequential'
   )
 ORDER BY APS.AppSummID DESC;
+
+
+---------------------------------------------------------------------------
+USE [OTS_DB]
+GO
+/****** Object:  StoredProcedure [dbo].[GetAppProcSummary]    Script Date: 10/23/2024 11:32:08 am ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	Procedure to fetch AppProc Summary based on Approver and UserID
+-- =============================================
+ALTER PROCEDURE [dbo].[GetAppProcSummary] 
+    @Approver INT,
+    @UserID INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT APS.AppSummID, APS.Approver, AT.AppType, APS.ReqDate, APS.DraftNum, 
+           SH.DocDate, APS.DocType, SH.CustomerName, SH.TotalAmtDue, APS.Remarks, 
+           APS.Status, AM.Type, APD2.*
+    FROM [OTS_DB].[dbo].[AppProc_Summary] APS
+    INNER JOIN [OTS_DB].[dbo].[AppProc_Main] AM ON APS.AppProcID = AM.AppProcID
+    INNER JOIN [OTS_DB].[dbo].[AppType] AT ON AT.AppTypeID = AM.AppTypeID
+    INNER JOIN [OTS_DB].[dbo].[SO_Header] SH ON APS.DraftNum = SH.DraftNum
+    INNER JOIN [OTS_DB].[dbo].[AppProc_DetApp] APD ON AM.AppProcID = APD.AppProcID
+    INNER JOIN [OTS_DB].[dbo].[AppProc_DetApp] APD2 ON AM.AppProcID = APD2.AppProcID
+    WHERE APS.Approver = @Approver
+    AND APD2.UserID = @UserID
+    AND (
+        (AM.Type = 'sequential' AND APD.AppLevel = (
+            SELECT MIN(APD3.AppLevel)
+            FROM [OTS_DB].[dbo].[AppProc_DetApp] APD3
+            WHERE APD3.AppProcID = AM.AppProcID
+        ))
+        OR AM.Type != 'sequential'
+    )
+    ORDER BY APS.AppSummID DESC;
+END
