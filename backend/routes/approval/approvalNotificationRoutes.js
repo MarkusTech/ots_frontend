223441e40894290abba79c7070ebc60aa18b9628
MarkusTech <wm.recopelacion@gmail.com>
2024-11-29 13:46:21 +0800

    Update approvalNotificationRoutes.js

--- backend/routes/approval/approvalNotificationRoutes.js
+++ backend/routes/approval/approvalNotificationRoutes.js
@@ -5,6 +5,7 @@ import {
   orignatorNotificationCount,
   approverList,
   approverListV2,
+  approverListV3,
   approverNotification,
 } from "../../controllers/approval/approvalNotification.js";
 const router = express.Router();
@@ -20,6 +21,7 @@ router.get(
 router.get("/approval-notification", approvalNotification);
 router.get("/approver-list/:approverID", approverList);
 router.get("/approver-list/v2/:approverID", approverListV2);
+router.get("/approver-list/v3/:approverID", approverListV3);
 router.get("/approver-notification/:approverID", approverNotification);
 
 export default router;
