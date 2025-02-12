481504b7cb31ddecaaa1762eb96e57e563c35336
MarkusTech <wm.recopelacion@gmail.com>
2025-02-12 13:53:38 +0800

    update

--- backend_retrieval/config/db.js
+++ backend_retrieval/config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const config = {
   database: process.env.MSSQL_DATABASE,
-  server: process.env.MSSQLL_SERVER,
+  server: process.env.MSSQL_SERVER,
   user: process.env.MSSQL_USERNAME,
   password: process.env.MSSQL_PASSWORD,
   options: {
